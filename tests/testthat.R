547244dac329ecd42f793bb9cf408695b45d40de
Merete Lutz <meretelutz@gmail.com>
2024-01-09 15:58:40 -0800

    Create unit tests

---
+++ tests/testthat.R
@@ -0,0 +1,14 @@
+# This file is part of the standard setup for testthat.
+# It is recommended that you do not modify it.
+#
+# Where should you do additional test configuration?
+# Learn more about the roles of various files in:
+# * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
+# * https://testthat.r-lib.org/articles/special-files.html
+
+library(testthat)
+library(regexcite)
+
+test_that("strsplit1() splits a string", {
+  expect_equal(strsplit1("a,b,c", split = ","), c("a", "b", "c"))
+})
